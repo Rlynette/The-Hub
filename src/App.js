713b9e2777e93305eaf646c528b373663cf9626b
Rlynette <lynette.ronnoh@student.moringaschool.com>
2023-03-24 09:28:33 +0300

    edit App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Header from "./components/header/Header";
+import { Route, Routes} from "react-router-dom;"
+import Home from "./components/home/Home"
 
 function App() {
   return (
     <div className="App">
       <h2>class</h2>
+      <Routes>
+        <Header />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/header" element={<Header />} />npm  */}
+      </Routes>
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import "./header.css";
+import {Link} from "react-router-dom";
 
 function Header() {
   return (
-    <div>
-      
+    <div className="header">
+      {/* <Link to="/"> */}
+      <h2>The-Hub</h2>
+      {/* </Link> */}
+
+      <nav>
+        <ul>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+
+          <Link to="/courses">
+            <li>Courses</li>
+          </Link>
+
+          <Link to="/recruitment">
+            <li>Recruitment</li>
+          </Link>
+
+          <Link to="/community">
+            <li>Community</li>
+          </Link>
+
+          <Link to="/blog">
+            <li>Blogs</li>
+          </Link>
+
+          <Link to="/apply">
+            <button>Apply Now</button>
+          </Link>
+          
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default Header
